2ece37e3941871bcc2829e1a5a289d9b83f15cd5
WANG Xuerui <wangxuerui@iscas.ac.cn>
2023-10-31 16:02:19 +0800

    build: add support for riscv64

---
+++ scripts/dist-image/prepare-distro.riscv64.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+
+groupadd -g "$BUILDER_GID" b
+useradd -d /home/b -m -g "$BUILDER_GID" -u "$BUILDER_UID" -s /bin/bash b
+
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
+# HTTPS needs ca-certificates to work
+sed -i 's@http://deb\.debian\.org/@http://mirrors.huaweicloud.com/@g' /etc/apt/sources.list.d/debian.sources
+
+# Non-interactive configuration of tzdata
+debconf-set-selections <<EOF
+tzdata tzdata/Areas select Etc
+tzdata tzdata/Zones/Etc select UTC
+EOF
+
+package_list=(
+    build-essential
+
+    # for Nuitka
+    python3.11-dev
+    zlib1g-dev  # likely for one-file builds
+    patchelf    # for one-file builds
+    ccache      # for rebuilds
+
+    # for the Python build env
+    python3.11-venv
+
+    # Python library deps
+    # cffi
+    libffi-dev
+    # cryptography
+    rustc
+    cargo
+    # Rust openssl-sys
+    libssl-dev
+    pkgconf
+)
+
+apt-get update
+# apt-get upgrade -qqy  # assume the base snapshots are reasonably up-to-date
+apt-get install -y "${package_list[@]}"
+
+apt-get clean
+
+python3.11 -m venv /home/b/venv
+/home/b/venv/bin/pip install -U pip setuptools wheel
+/home/b/venv/bin/pip install poetry
+chown -R "$BUILDER_UID:$BUILDER_GID" /home/b/venv
+
+# remove wheel caches in the root user
+rm -rf /root/.cache
