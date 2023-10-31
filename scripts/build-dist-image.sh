5400d03256775db102fb1c8275a43a91a52d8c47
WANG Xuerui <wangxuerui@iscas.ac.cn>
2023-10-31 15:54:29 +0800

    build: prepare for dist-ing for alternative arches

--- scripts/build-dist-image.sh
+++ scripts/build-dist-image.sh
@@ -4,5 +4,17 @@ set -e
 
 MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+arch="$1"
+if [[ -z $arch ]]; then
+    echo "usage: $0 <arch>" >&2
+    echo >&2
+    echo "see scripts/dist-image for available arch choices" >&2
+    exit 1
+fi
+
 cd "$MY_DIR/dist-image"
-exec docker build --rm -t ruyi-python-dist:ubuntu2204-20231029 .
+exec docker build --rm \
+    --platform "linux/${arch}" \
+    -t "ruyi-python-dist:20231031-${arch}" \
+    -f "Dockerfile.${arch}" \
+    .
--- scripts/dist.sh
+++ scripts/dist.sh
@@ -6,16 +6,25 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
 
 cd "$REPO_ROOT"
 
-BUILD_DIR="$REPO_ROOT/tmp/build"
-POETRY_CACHE_DIR="$REPO_ROOT/tmp/poetry-cache"
+arch="$1"
+if [[ -z $arch ]]; then
+    echo "usage: $0 <arch>" >&2
+    echo >&2
+    echo "see scripts/dist-image for available arch choices" >&2
+    exit 1
+fi
+
+BUILD_DIR="$REPO_ROOT/tmp/build.${arch}"
+POETRY_CACHE_DIR="$REPO_ROOT/tmp/poetry-cache.${arch}"
 mkdir -p "$BUILD_DIR" "$POETRY_CACHE_DIR"
 
 docker_args=(
     --rm
+    --platform "linux/${arch}"
     -v "$REPO_ROOT":/home/b/ruyi:ro
     -v "$BUILD_DIR":/build
     -v "$POETRY_CACHE_DIR":/poetry-cache
-    -t ruyi-python-dist:ubuntu2204-20231029
+    -t "ruyi-python-dist:20231031-${arch}"
     /home/b/ruyi/scripts/dist-inner.sh
 )
 
