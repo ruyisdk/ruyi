01b41afdf1d4dd2d02b84aa25e6200d71cb16481
WANG Xuerui <wangxuerui@iscas.ac.cn>
2023-11-07 17:48:23 +0800

    feat: rewrite mux to be venv-aware

--- ruyi/mux/runtime.py
+++ ruyi/mux/runtime.py
@@ -1,60 +1,32 @@
 import os
-import pathlib
-import sys
-from typing import List, NoReturn, Optional, Union
-
-from rich import print
+import shlex
+from typing import List, NoReturn
 
 from .. import log
+from ..config import RuyiVenvConfig
 
 
-def mux_main(argv: List[str]) -> Union[int, NoReturn]:
-    log.D(f"mux mode: argv = {argv}")
+def mux_main(argv: List[str]) -> int | NoReturn:
+    basename = os.path.basename(argv[0])
+    log.D(f"mux mode: argv = {argv}, basename = {basename}")
 
-    # if no preference is set for the cwd, find the next command in PATH with
-    # the same argv[0], and exec it
-    next = find_next_in_path(os.path.basename(argv[0]))
-    if next is None:
-        log.F(f"cannot find a '[yellow]{argv[0]}[/yellow]' to exec")
-        return 127
+    vcfg = RuyiVenvConfig.load_from_venv()
+    if vcfg is None:
+        log.F("the Ruyi toolchain mux is not configured")
+        log.I("check out `ruyi venv` for making a virtual environment")
+        return 1
 
-    new_argv = [next] + argv[1:]
-    log.D(f"exec-ing [green]{next}[/green] with argv {new_argv}")
-    return os.execv(next, new_argv)
+    binpath = os.path.join(vcfg.toolchain_bindir, basename)
+    common_argv_to_insert = shlex.split(vcfg.profile_common_flags)
 
+    log.D(f"binary to exec: {binpath}")
+    log.D(f"parsed profile flags: {common_argv_to_insert}")
 
-# This is intended to resemble shutil.which, but knows to skip ruyi itself to
-# avoid infinite looping.
-def find_next_in_path(
-    argv0: str, search_paths: Optional[List[str]] = None
-) -> Optional[str]:
-    from ..cli import _self_exe
+    new_argv = [binpath]
+    if common_argv_to_insert:
+        new_argv.extend(common_argv_to_insert)
+    if len(argv) > 1:
+        new_argv.extend(argv[1:])
 
-    if search_paths is None:
-        path_str = os.environ.get("PATH", os.defpath)
-        if not path_str:
-            return None
-        search_paths = path_str.split(":")
-
-    # this is mirroring shutil.which logic, but skips the PATHEXT handling for
-    # now.
-    seen = set()
-    for dir in search_paths:
-        normalized = os.path.normcase(dir)
-        if normalized in seen:
-            continue
-
-        seen.add(normalized)
-        p = os.path.join(dir, argv0)
-        if is_executable(p) and not os.path.samefile(p, _self_exe):
-            return p
-
-    return None
-
-
-def is_executable(p: Union[str, pathlib.Path]) -> bool:
-    p = p if isinstance(p, pathlib.Path) else pathlib.Path(p)
-    try:
-        return os.access(p, os.F_OK | os.X_OK)
-    except FileNotFoundError:
-        return False
+    log.D(f"exec-ing with argv {new_argv}")
+    return os.execv(binpath, new_argv)
