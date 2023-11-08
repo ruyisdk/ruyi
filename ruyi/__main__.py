c2224c3973877e40994efdf6373dda08ed8fa035
WANG Xuerui <wangxuerui@iscas.ac.cn>
2023-11-08 17:12:57 +0800

    fix: fix self executable location detection of Nuitka one-file builds

--- ruyi/__main__.py
+++ ruyi/__main__.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
+    import os.path
     import sys
-    from ruyi import record_self_exe
+    import ruyi
     from ruyi.cli import main
 
     # note down our own executable path, for identity-checking in mux
-    record_self_exe(__file__)
+    # we assume the one-file build if Nuitka is detected
+    self_exe = (
+        os.path.abspath(sys.argv[0]) if hasattr(ruyi, "__compiled__") else __file__
+    )
+    ruyi.record_self_exe(self_exe)
 
     sys.exit(main(sys.argv))
