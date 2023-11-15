a89b55a95da78277f30a532b8d90c9ac963525ad
WANG Xuerui <wangxuerui@iscas.ac.cn>
2023-11-15 17:54:36 +0800

    build: bump self version to 0.2.0-alpha.20231115

--- scripts/dist-inner.sh
+++ scripts/dist-inner.sh
@@ -16,8 +16,8 @@ nuitka_args=(
     --output-filename=ruyi
     --output-dir=/build
     --no-deployment-flag=self-execution
-    --product-version=0.1.0
-    --onefile-tempdir-spec="%CACHE_DIR%/ruyi/progcache/%VERSION%"
+    --product-version=0.1.50.20231115
+    --onefile-tempdir-spec="%CACHE_DIR%/ruyi/progcache/0.2.0-alpha.20231115"
     ./ruyi/__main__.py
 )
 
