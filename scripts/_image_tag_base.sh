e110f610e5aac6f90a845604377905468c070e91
WANG Xuerui <wangxuerui@iscas.ac.cn>
2023-12-01 18:12:44 +0800

    build: support disting for arm64

--- scripts/_image_tag_base.sh
+++ scripts/_image_tag_base.sh
@@ -7,11 +7,14 @@ image_tag_base() {
         amd64)
             echo "ruyi-python-dist:20231125"
             ;;
+        arm64)
+            echo "ruyi-pythin-dist:20231201"
+            ;;
         riscv64)
             echo "ruyi-python-dist:20231125"
             ;;
         *)
-            echo "error: unsupported arch $arch; supported are: amd64, riscv64" >&2
+            echo "error: unsupported arch $arch; supported are: amd64, arm64, riscv64" >&2
             return 1
             ;;
     esac
