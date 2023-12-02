154abd89bd0a2289335fef31191c3cecb0259a10
WANG Xuerui <wangxuerui@iscas.ac.cn>
2023-12-02 18:42:19 +0800

    feat: wire up uninstallation

--- ruyi/cli/self_cli.py
+++ ruyi/cli/self_cli.py
@@ -1,7 +1,11 @@
 import argparse
+import os
+import shutil
 
 import ruyi
+from .. import config
 from .. import log
+from . import user_input
 
 
 def cli_self_uninstall(args: argparse.Namespace) -> int:
@@ -15,6 +19,21 @@ def cli_self_uninstall(args: argparse.Namespace) -> int:
         )
         return 1
 
-    # TODO
+    if not consent:
+        if not user_input.ask_for_yesno_confirmation("Continue?"):
+            log.I("aborting uninstallation")
+            return 0
+    else:
+        log.I("uninstallation consent given over CLI, proceeding")
+
+    if purge:
+        log.I("removing the Ruyi cache")
+        cfg = config.GlobalConfig.load_from_config()
+        shutil.rmtree(cfg.cache_root, True)
+
+    log.I("removing the ruyi binary")
+    os.unlink(ruyi.self_exe())
+
+    log.I("[yellow]ruyi[/yellow] is uninstalled")
 
     return 0
