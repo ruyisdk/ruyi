78b7c0067b840e792559db403dd3c0ff3d06e88d
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-01-18 16:04:34 +0800

    style: black-ify stubs

--- stubs/frontmatter/__init__.pyi
+++ stubs/frontmatter/__init__.pyi
@@ -3,19 +3,42 @@ from .default_handlers import BaseHandler
 from _typeshed import Incomplete
 from typing import Iterable
 
-__all__ = ['parse', 'load', 'loads', 'dump', 'dumps']
+__all__ = ["parse", "load", "loads", "dump", "dumps"]
 
-def parse(text: str, encoding: str = 'utf-8', handler: BaseHandler | None = None, **defaults: object) -> tuple[dict[str, object], str]: ...
-def load(fd: str | io.IOBase, encoding: str = 'utf-8', handler: BaseHandler | None = None, **defaults: object) -> Post: ...
-def loads(text: str, encoding: str = 'utf-8', handler: BaseHandler | None = None, **defaults: object) -> Post: ...
-def dump(post: Post, fd: str | io.IOBase, encoding: str = 'utf-8', handler: BaseHandler | None = None, **kwargs: object) -> None: ...
+def parse(
+    text: str,
+    encoding: str = "utf-8",
+    handler: BaseHandler | None = None,
+    **defaults: object,
+) -> tuple[dict[str, object], str]: ...
+def load(
+    fd: str | io.IOBase,
+    encoding: str = "utf-8",
+    handler: BaseHandler | None = None,
+    **defaults: object,
+) -> Post: ...
+def loads(
+    text: str,
+    encoding: str = "utf-8",
+    handler: BaseHandler | None = None,
+    **defaults: object,
+) -> Post: ...
+def dump(
+    post: Post,
+    fd: str | io.IOBase,
+    encoding: str = "utf-8",
+    handler: BaseHandler | None = None,
+    **kwargs: object,
+) -> None: ...
 def dumps(post: Post, handler: BaseHandler | None = None, **kwargs: object) -> str: ...
 
 class Post:
     content: str
     metadata: dict[str, object]
     handler: BaseHandler | None
-    def __init__(self, content: str, handler: BaseHandler | None = None, **metadata: object) -> None: ...
+    def __init__(
+        self, content: str, handler: BaseHandler | None = None, **metadata: object
+    ) -> None: ...
     def __getitem__(self, name: str) -> object: ...
     def __contains__(self, item: object) -> bool: ...
     def __setitem__(self, name: str, value: object) -> None: ...
--- stubs/frontmatter/default_handlers.pyi
+++ stubs/frontmatter/default_handlers.pyi
@@ -3,13 +3,18 @@ from _typeshed import Incomplete
 from frontmatter import Post
 from typing import Any
 
-__all__ = ['BaseHandler', 'YAMLHandler', 'JSONHandler', 'TOMLHandler']
+__all__ = ["BaseHandler", "YAMLHandler", "JSONHandler", "TOMLHandler"]
 
 class BaseHandler:
     FM_BOUNDARY: re.Pattern[str] | None
     START_DELIMITER: str | None
     END_DELIMITER: str | None
-    def __init__(self, fm_boundary: re.Pattern[str] | None = None, start_delimiter: str | None = None, end_delimiter: str | None = None) -> None: ...
+    def __init__(
+        self,
+        fm_boundary: re.Pattern[str] | None = None,
+        start_delimiter: str | None = None,
+        end_delimiter: str | None = None,
+    ) -> None: ...
     def detect(self, text: str) -> bool: ...
     def split(self, text: str) -> tuple[str, str]: ...
     def load(self, fm: str) -> dict[str, Any]: ...
