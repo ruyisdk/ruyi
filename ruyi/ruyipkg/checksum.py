ruyisdk/ruyi

commit 47dcdae13359e7df2acb86f01f68353250205dcf

refactor(ruyipkg): fix typing of checksum.py

ruyi/ruyipkg/checksum.py
@@ -4,7 +4,7 @@ from typing import BinaryIO, Iterable
 SUPPORTED_CHECKSUM_KINDS = {"sha256", "sha512"}
 
 
-def get_hash_instance(kind: str):
+def get_hash_instance(kind: str) -> hashlib._Hash:
     if kind not in SUPPORTED_CHECKSUM_KINDS:
         raise ValueError(f"checksum algorithm {kind} not supported")
     return hashlib.new(kind)
@@ -26,7 +26,7 @@ class Checksummer:
     def compute(
         self,
         kinds: Iterable[str] | None = None,
-        chunksize=4096,
+        chunksize: int = 4096,
     ) -> dict[str, str]:
         if kinds is None:
             kinds = self.checksums.keys()

commit fcf85eb2485c1aec18736ba359657f6c219e83d7

fix(ruyipkg): hashlib._Hash cannot be used outside of type-checking
Fixes: 47dcdae ("refactor(ruyipkg): fix typing of checksum.py")

ruyi/ruyipkg/checksum.py
@@ -4,7 +4,7 @@ from typing import BinaryIO, Iterable
 SUPPORTED_CHECKSUM_KINDS = {"sha256", "sha512"}
 
 
-def get_hash_instance(kind: str) -> hashlib._Hash:
+def get_hash_instance(kind: str) -> "hashlib._Hash":
     if kind not in SUPPORTED_CHECKSUM_KINDS:
         raise ValueError(f"checksum algorithm {kind} not supported")
     return hashlib.new(kind)