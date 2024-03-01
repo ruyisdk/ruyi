3f4cc6b0dd996e2c5b234a8f66e33473564d9a39
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-03-01 18:08:01 +0800

    build: re-use dist-inner.py for linux dist builds
    So that we only have one place to handle Nuitka options. This means a
    shebang line has to be added to the python file.

--- scripts/dist-inner.sh
+++ scripts/dist-inner.sh
@@ -16,23 +16,4 @@ pushd /home/b/venv/lib/python*/site-packages > /dev/null
 patch -Np1 < /home/b/ruyi/scripts/patches/0001-Onefile-Respect-XDG_CACHE_HOME-when-rendering-CACHE_.patch
 popd > /dev/null
 
-eval "$(./scripts/_dist_version_helper.py)"
-
-echo "Project SemVer       : $RUYI_DIST_SEMVER"
-echo "Nuitka version to use: $RUYI_DIST_NUITKA_VER"
-
-nuitka_args=(
-    --standalone
-    --onefile
-    --output-filename=ruyi
-    --output-dir=/build
-    --no-deployment-flag=self-execution
-    --product-version="$RUYI_DIST_NUITKA_VER"
-    --onefile-tempdir-spec="{CACHE_DIR}/ruyi/progcache/${RUYI_DIST_SEMVER}"
-    --include-package=pygments.formatters
-    --include-package=pygments.lexers
-    --include-package=pygments.styles
-    ./ruyi/__main__.py
-)
-
-python -m nuitka "${nuitka_args[@]}"
+./scripts/dist-inner.py
