ruyisdk/ruyi

commit fdd9930d746187296533f7b646f1643ccb156c2e

refactor: migrate to pygit2 for Git operations
Unfortunately this requires elimination of the star-import from _pygit2.

fix: fix pygit2 dist-bundling and initialization

ruyi/utils/git.py
@@ -1,0 +1,113 @@
+from contextlib import AbstractContextManager
+from typing import Any, Self
+
+from pygit2 import Oid
+from pygit2.callbacks import RemoteCallbacks
+from pygit2.enums import MergeAnalysis
+from pygit2.remotes import TransferProgress
+from pygit2.repository import Repository
+from rich.progress import Progress, TaskID
+from rich.text import Text
+
+from .. import log
+
+
+class RemoteGitProgressIndicator(
+    RemoteCallbacks,
+    AbstractContextManager["RemoteGitProgressIndicator"],
+):
+    def __init__(self) -> None:
+        super().__init__()
+        self.p = Progress()
+        self.task: TaskID | None = None
+        self._last_stats: TransferProgress | None = None
+        self._task_name: str = ""
+
+    def __enter__(self) -> Self:
+        self.p.__enter__()
+        return self
+
+    def __exit__(self, exc_type: Any, exc_value: Any, tb: Any) -> None:
+        return self.p.__exit__(exc_type, exc_value, tb)
+
+    def sideband_progress(self, string: str) -> None:
+        self.p.console.print("\r", Text(string), sep="", end="")
+
+    def transfer_progress(self, stats: TransferProgress) -> None:
+        new_phase = False
+        task_name: str = self._task_name
+        total: int = 0
+        completed: int = 0
+
+        if (
+            self._last_stats is None
+            or self._last_stats.received_objects != stats.received_objects
+        ):
+            task_name = "transferring objects"
+            total = stats.total_objects
+            completed = stats.received_objects
+        elif self._last_stats.indexed_deltas != stats.indexed_deltas:
+            task_name = "processing deltas"
+            total = stats.total_deltas
+            completed = stats.indexed_deltas
+        elif self._last_stats.received_bytes != stats.received_bytes:
+            # we don't render the received size at the moment
+            pass
+
+        new_phase = self._task_name != task_name
+        if new_phase:
+            self.task = self.p.add_task(task_name, total=total, completed=completed)
+            self._task_name = task_name
+        else:
+            if self.task is not None:
+                self.p.update(self.task, total=total, completed=completed)
+
+        self._last_stats = stats
+
+
+# based on https://stackoverflow.com/questions/27749418/implementing-pull-with-pygit2
+def pull_ff_or_die(
+    repo: Repository,
+    remote_name: str,
+    remote_url: str,
+    branch_name: str,
+) -> None:
+    remote = repo.remotes[remote_name]
+    if remote.url != remote_url:
+        log.D(f"updating url of remote {remote_name} from {remote.url} to {remote_url}")
+        repo.remotes.set_url("origin", remote_url)
+
+    log.D("fetching")
+    with RemoteGitProgressIndicator() as pr:
+        remote.fetch(callbacks=pr)
+
+    remote_head_ref = repo.lookup_reference(f"refs/remotes/{remote_name}/{branch_name}")
+    remote_head: Oid
+    if isinstance(remote_head_ref.target, Oid):
+        remote_head = remote_head_ref.target
+    else:
+        assert isinstance(remote_head_ref.target, str)
+        remote_head = Oid(hex=remote_head_ref.target)
+
+    merge_analysis, _ = repo.merge_analysis(remote_head)
+    match merge_analysis:
+        case MergeAnalysis.UP_TO_DATE:
+            # nothing to do
+            log.D("repo state already up-to-date")
+            return
+        case MergeAnalysis.UNBORN | MergeAnalysis.FASTFORWARD:
+            # simple fast-forwarding is enough in both cases
+            log.D(f"fast-forwarding repo to {remote_head.hex}")
+            tgt = repo.get(remote_head)
+            assert tgt is not None
+            repo.checkout_tree(tgt)
+
+            log.D(f"updating branch {branch_name} HEAD")
+            local_branch_ref = repo.lookup_reference(f"refs/heads/{branch_name}")
+            local_branch_ref.set_target(remote_head)
+            repo.head.set_target(remote_head)
+        case MergeAnalysis.NONE | MergeAnalysis.NORMAL:
+            # cannot handle these cases
+            log.F("cannot fast-forward repo to newly fetched state")
+            log.I("manual intervention is required to avoid data loss")
+            raise SystemExit(1)

ruyi/__main__.py
@@ -4,6 +4,11 @@ if __name__ == "__main__":
     import sys
     import ruyi
     from ruyi import log
+
+    # this must happen before pygit2 is imported
+    from ruyi.utils import ssl_patch
+
+    del ssl_patch
     from ruyi.cli import init_debug_status, main
     from ruyi.cli.nuitka import get_nuitka_self_exe
 

ruyi/cli/__init__.py
@@ -6,7 +6,6 @@ from typing import Callable, List
 import ruyi
 from .. import log
 from ..mux.runtime import mux_main
-from .prereqs import check_dep_binaries
 
 RUYI_ENTRYPOINT_NAME = "ruyi"
 
@@ -280,8 +279,6 @@ def init_argparse() -> argparse.ArgumentParser:
 def main(argv: List[str]) -> int:
     if not is_called_as_ruyi(argv[0]):
         return mux_main(argv)
-
-    check_dep_binaries()
 
     p = init_argparse()
     args = p.parse_args(argv[1:])

ruyi/cli/prereqs.py
@@ -5,19 +5,6 @@ from typing import NoReturn
 from ruyi import log
 
 
-def ensure_git_binary() -> None | NoReturn:
-    try:
-        import git as _
-    except ImportError:
-        log.F(
-            "seems [yellow]git[/yellow] is not available, which [yellow]ruyi[/yellow] requires"
-        )
-        log.I("please install Git and retry")
-        sys.exit(1)
-
-    return None
-
-
 def has_cmd_in_path(cmd: str) -> bool:
     return shutil.which(cmd) is not None
 
@@ -57,9 +44,3 @@ def ensure_cmds(*cmds: str) -> None | NoReturn:
     )
     log.I("please install and retry")
     sys.exit(1)
-
-
-def check_dep_binaries() -> None:
-    ensure_git_binary()
-    # init_cmd_presence_map() is called on-demand, to avoid having to reach out
-    # to FS at every `ruyi` invocation which can get expensive.

ruyi/ruyipkg/repo.py
@@ -3,11 +3,12 @@ import json
 import os.path
 from typing import Any, Iterable, NotRequired, Tuple, TypedDict, TypeGuard
 
-from git import Repo
+from pygit2 import clone_repository
+from pygit2.repository import Repository
 import yaml
 
 from .. import log
-from ..utils.git_progress import RemoteGitProgressIndicator
+from ..utils.git import RemoteGitProgressIndicator, pull_ff_or_die
 from .news import NewsItem
 from .pkg_manifest import is_prerelease, PackageManifest
 from .profile import ArchProfilesDeclType, ProfileDecl, parse_profiles
@@ -34,7 +35,7 @@ class MetadataRepo:
         self.root = path
         self.remote = remote
         self.branch = branch
-        self.repo: Repo | None = None
+        self.repo: Repository | None = None
 
         self._pkgs: dict[str, dict[str, PackageManifest]] = {}
         self._categories: dict[str, dict[str, dict[str, PackageManifest]]] = {}
@@ -43,44 +44,29 @@ class MetadataRepo:
         self._news_cache: list[NewsItem] | None = None
         self._provisioner_config_cache: tuple[ProvisionerConfig | None] | None = None
 
-    def ensure_git_repo(self) -> Repo:
+    def ensure_git_repo(self) -> Repository:
         if self.repo is not None:
             return self.repo
 
         if os.path.exists(self.root):
-            self.repo = Repo(self.root)
+            self.repo = Repository(self.root)
             return self.repo
 
         log.D(f"{self.root} does not exist, cloning from {self.remote}")
 
         with RemoteGitProgressIndicator() as pr:
-            self.repo = Repo.clone_from(
+            self.repo = clone_repository(
                 self.remote,
                 self.root,
-                branch=self.branch,
-                progress=pr.update,
+                checkout_branch=self.branch,
+                callbacks=pr,
             )
 
         return self.repo
 
     def sync(self) -> None:
         repo = self.ensure_git_repo()
-        remote = repo.remote()
-        if remote.url != self.remote:
-            log.D(f"updating remote url from {remote.url} to {self.remote}")
-            remote.set_url(self.remote, remote.url)
-        log.D("fetching")
-        with RemoteGitProgressIndicator() as pr:
-            remote.fetch(progress=pr)
-        # cosmetic touch-up: sync the local head reference to the remote HEAD too
-        main_branch = repo.heads[self.branch]
-        tgt_commit = remote.refs[self.branch].commit
-        log.D(
-            f"updating branch {self.branch} head {main_branch} to commit {tgt_commit}"
-        )
-        main_branch.commit = tgt_commit
-        log.D("checking out")
-        main_branch.checkout(force=True)
+        return pull_ff_or_die(repo, "origin", self.remote, self.branch)
 
     def get_config(self) -> RepoConfigType:
         self.ensure_git_repo()

ruyi/utils/git_progress.py
@@ -1,72 +1,0 @@
-from typing import Any, Self
-
-from git.remote import RemoteProgress
-from rich.progress import Progress, TaskID
-
-from .. import log
-
-
-def is_phase_beginning(op: int) -> bool:
-    return op & RemoteProgress.BEGIN != 0
-
-
-def is_phase_ending(op: int) -> bool:
-    return op & RemoteProgress.END != 0
-
-
-def get_phase_name(op: int) -> str:
-    match op & RemoteProgress.OP_MASK:
-        case RemoteProgress.COUNTING:
-            return "Counting"
-        case RemoteProgress.COMPRESSING:
-            return "Compressing"
-        case RemoteProgress.WRITING:
-            return "Writing"
-        case RemoteProgress.RECEIVING:
-            return "Receiving"
-        case RemoteProgress.RESOLVING:
-            return "Resolving"
-        case RemoteProgress.FINDING_SOURCES:
-            return "Finding sources"
-        case RemoteProgress.CHECKING_OUT:
-            return "Checking out"
-        case _:
-            return f"GitPython phase {op}"
-
-
-class RemoteGitProgressIndicator(RemoteProgress):
-    def __init__(self) -> None:
-        super().__init__()
-        self.p = Progress()
-        self.task: TaskID | None = None
-
-    def __enter__(self) -> Self:
-        self.p.__enter__()
-        return self
-
-    def __exit__(self, exc_type: Any, exc_value: Any, tb: Any) -> None:
-        return self.p.__exit__(exc_type, exc_value, tb)
-
-    def _start(self, op: int, max_count: float | None) -> None:
-        self.task = self.p.add_task(get_phase_name(op), total=max_count)
-
-    def _bump_to(self, val: float) -> None:
-        if self.task is None:
-            return
-        self.p.update(self.task, completed=val)
-
-    def update(
-        self,
-        op_code: int,
-        cur_count: str | float,
-        max_count: str | float | None = None,
-        message: str = "",
-    ) -> None:
-        cur_count = cur_count if isinstance(cur_count, float) else 0.0
-        max_count = max_count if isinstance(max_count, float) else None
-        if is_phase_beginning(op_code):
-            if message:
-                log.I(message)
-            self._start(op_code, max_count)
-        else:
-            self._bump_to(cur_count)

ruyi/utils/ssl_patch.py
@@ -1,0 +1,79 @@
+import ctypes
+import os
+import ssl
+import sys
+
+from .. import log
+
+_orig_get_default_verify_paths = ssl.get_default_verify_paths
+_cached_paths: ssl.DefaultVerifyPaths | None = None
+
+
+def get_system_ssl_default_verify_paths() -> ssl.DefaultVerifyPaths:
+    global _cached_paths
+
+    orig_paths = _orig_get_default_verify_paths()
+    if sys.platform != "linux":
+        return orig_paths
+
+    if _cached_paths is not None:
+        return _cached_paths
+
+    # imitate the stdlib flow but with overridden data source
+    try:
+        parts = _query_linux_system_ssl_default_cert_paths()
+    except Exception as e:
+        log.D(f"cannot get system libssl default cert paths: {e}")
+        return _orig_get_default_verify_paths()
+
+    cafile = os.environ.get(parts[0], parts[1])
+    capath = os.environ.get(parts[2], parts[3])
+
+    # must do "else None" like the stdlib, despite the type annotation being just "str"
+    result = ssl.DefaultVerifyPaths(
+        cafile if os.path.isfile(cafile) else None,  # type: ignore[arg-type]
+        capath if os.path.isdir(capath) else None,  # type: ignore[arg-type]
+        *parts,
+    )
+
+    if result != orig_paths:
+        log.D(
+            "get_default_verify_paths() values differ between bundled and system libssl"
+        )
+        log.D(f"bundled: {orig_paths}")
+        log.D(f" system: {result}")
+
+    _cached_paths = result
+    return result
+
+
+def _decode_fsdefault_or_none(val: int | None) -> str:
+    if val is None:
+        return ""
+
+    s = ctypes.c_char_p(val)
+    if s.value is None:
+        return ""
+
+    return s.value.decode(sys.getfilesystemencoding())
+
+
+def _query_linux_system_ssl_default_cert_paths() -> tuple[str, str, str, str]:
+    # this can work because right now Nuitka packages the libssl as "libssl.so.X"
+    # notice the presence of sover suffix
+    # so dlopen-ing "libssl.so" will get us the system library
+    libssl = ctypes.CDLL("libssl.so")
+    libssl.X509_get_default_cert_file_env.restype = ctypes.c_void_p
+    libssl.X509_get_default_cert_file.restype = ctypes.c_void_p
+    libssl.X509_get_default_cert_dir_env.restype = ctypes.c_void_p
+    libssl.X509_get_default_cert_dir.restype = ctypes.c_void_p
+
+    return (
+        _decode_fsdefault_or_none(libssl.X509_get_default_cert_file_env()),
+        _decode_fsdefault_or_none(libssl.X509_get_default_cert_file()),
+        _decode_fsdefault_or_none(libssl.X509_get_default_cert_dir_env()),
+        _decode_fsdefault_or_none(libssl.X509_get_default_cert_dir()),
+    )
+
+
+ssl.get_default_verify_paths = get_system_ssl_default_verify_paths

scripts/dist-inner.py
@@ -30,6 +30,7 @@ def main() -> None:
         "--include-package=pygments.formatters",
         "--include-package=pygments.lexers",
         "--include-package=pygments.styles",
+        "--include-package=_cffi_backend",  # https://github.com/Nuitka/Nuitka/issues/2505
         "--windows-icon-from-ico=resources/ruyi.ico",
         "./ruyi/__main__.py",
     )

commit 3b3c5b5e48afac1b3548f3a56751682095d391d0

fix: fix handling of pygit2 merge analysis result
Fixes: fdd9930 ("refactor: migrate to pygit2 for Git operations")

ruyi/utils/git.py
@@ -90,24 +90,26 @@ def pull_ff_or_die(
         remote_head = Oid(hex=remote_head_ref.target)
 
     merge_analysis, _ = repo.merge_analysis(remote_head)
-    match merge_analysis:
-        case MergeAnalysis.UP_TO_DATE:
-            # nothing to do
-            log.D("repo state already up-to-date")
-            return
-        case MergeAnalysis.UNBORN | MergeAnalysis.FASTFORWARD:
-            # simple fast-forwarding is enough in both cases
-            log.D(f"fast-forwarding repo to {remote_head.hex}")
-            tgt = repo.get(remote_head)
-            assert tgt is not None
-            repo.checkout_tree(tgt)
 
-            log.D(f"updating branch {branch_name} HEAD")
-            local_branch_ref = repo.lookup_reference(f"refs/heads/{branch_name}")
-            local_branch_ref.set_target(remote_head)
-            repo.head.set_target(remote_head)
-        case MergeAnalysis.NONE | MergeAnalysis.NORMAL:
-            # cannot handle these cases
-            log.F("cannot fast-forward repo to newly fetched state")
-            log.I("manual intervention is required to avoid data loss")
-            raise SystemExit(1)
+    if merge_analysis & MergeAnalysis.UP_TO_DATE:
+        # nothing to do
+        log.D("repo state already up-to-date")
+        return
+
+    if merge_analysis & (MergeAnalysis.UNBORN | MergeAnalysis.FASTFORWARD):
+        # simple fast-forwarding is enough in both cases
+        log.D(f"fast-forwarding repo to {remote_head.hex}")
+        tgt = repo.get(remote_head)
+        assert tgt is not None
+        repo.checkout_tree(tgt)
+
+        log.D(f"updating branch {branch_name} HEAD")
+        local_branch_ref = repo.lookup_reference(f"refs/heads/{branch_name}")
+        local_branch_ref.set_target(remote_head)
+        repo.head.set_target(remote_head)
+        return
+
+    # cannot handle these cases
+    log.F("cannot fast-forward repo to newly fetched state")
+    log.I("manual intervention is required to avoid data loss")
+    raise SystemExit(1)