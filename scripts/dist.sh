ruyisdk/ruyi

commit e941280bc7b72ea57cd3be6d8622517caa9ca10f

build: only ask for pty in dist.sh if running interactively
See: https://github.com/ruyisdk/ruyi/issues/9

scripts/dist.sh
@@ -28,7 +28,16 @@ docker_args=(
     -v "$BUILD_DIR":/build
     -v "$POETRY_CACHE_DIR":/poetry-cache
     -v "$CCACHE_DIR":/ccache
-    -ti "$(image_tag_base "$arch")-${arch}"
+)
+
+# only allocate pty if currently running interactively
+# check if stdout is a tty
+if [[ -t 1 ]]; then
+    docker_args+=( -t )
+fi
+
+docker_args+=(
+    "$(image_tag_base "$arch")-${arch}"
     /home/b/ruyi/scripts/dist-inner.sh
 )
 

commit 812b24f2781535f67fb7087fda4faf7c118d36f6

build: re-allow interrupting the dist process
Commit e941280 ("build: only ask for pty in dist.sh if running
interactively") inadvertently broke commit 37c875a ("build: allow
interrupting the dist process").

See: https://stackoverflow.com/questions/43099116/error-the-input-device-is-not-a-tty

scripts/dist.sh
@@ -73,6 +73,7 @@ mkdir -p "$BUILD_DIR" "$POETRY_CACHE_DIR" "$CCACHE_DIR" "$RUYI_DIST_CACHE_DIR"
 
 docker_args=(
     --rm
+    -i  # required to be able to interrupt the build with ^C
     --platform "linux/${arch}"
     -v "$REPO_ROOT":/home/b/ruyi
     -v "$BUILD_DIR":/build