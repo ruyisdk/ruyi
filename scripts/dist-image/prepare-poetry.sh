56ec47483353db536c06471063e8b6d2c5f06791
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-06-21 18:20:39 +0800

    build: bump dist image, add {maturin, auditwheel, cibuildwheel} to poetry-venv

--- scripts/dist-image/prepare-poetry.sh
+++ scripts/dist-image/prepare-poetry.sh
@@ -9,6 +9,7 @@ export MAKEFLAGS="-j$(nproc)"
 python3.12 -m venv /home/b/poetry-venv
 /home/b/poetry-venv/bin/pip install -U pip setuptools wheel
 /home/b/poetry-venv/bin/pip install poetry
+/home/b/poetry-venv/bin/pip install maturin==1.6.0 cibuildwheel==2.19.1 auditwheel==6.0.0
 ln -s /home/b/poetry-venv/bin/poetry /usr/local/bin/poetry
 
 python3.12 -m venv /home/b/venv
