3864629ceb395103c3f4234e3f1e6af5c9aad9f3
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-06-23 16:28:55 +0800

    build: bump dist image, assorted venv refactoring

--- scripts/dist-image/prepare-poetry.sh
+++ scripts/dist-image/prepare-poetry.sh
@@ -6,13 +6,16 @@ export MAKEFLAGS="-j$(nproc)"
 
 # poetry should be put into its own venv to avoid contaminating the dist build
 # venv; otherwise nuitka can and will see additional imports leading to bloat
-python3.12 -m venv /home/b/poetry-venv
-/home/b/poetry-venv/bin/pip install -U pip setuptools wheel
-/home/b/poetry-venv/bin/pip install poetry
-/home/b/poetry-venv/bin/pip install maturin==1.6.0 cibuildwheel==2.19.1 auditwheel==6.0.0
-ln -s /home/b/poetry-venv/bin/poetry /usr/local/bin/poetry
+python3.12 -m venv /home/b/build-tools-venv
+/home/b/build-tools-venv/bin/pip install -U pip setuptools wheel
+/home/b/build-tools-venv/bin/pip install poetry
+/home/b/build-tools-venv/bin/pip install maturin==1.6.0 cibuildwheel==2.19.1 auditwheel==6.0.0
+for tool in poetry maturin cibuildwheel auditwheel; do
+    ln -s /home/b/build-tools-venv/bin/"$tool" /usr/local/bin/"$tool"
+done
 
 python3.12 -m venv /home/b/venv
+/home/b/venv/bin/pip install -U pip setuptools wheel
 chown -R "$BUILDER_UID:$BUILDER_GID" /home/b/venv
 
 # remove wheel caches in the root user
