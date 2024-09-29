4f453a2fa0e8981280a7a5f7633879efdbf84f63
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-09-29 19:01:04 +0800

    feat(cli): show externally-managed status in `ruyi version` output

--- ruyi/cli/version_cli.py
+++ ruyi/cli/version_cli.py
@@ -8,7 +8,13 @@ from ..version import COPYRIGHT_NOTICE, RUYI_SEMVER
 def cli_version(gc: GlobalConfig, args: argparse.Namespace) -> int:
     from ..ruyipkg.host import get_native_host
 
-    print(f"Ruyi {RUYI_SEMVER}\n\nRunning on {get_native_host()}.\n")
+    print(f"Ruyi {RUYI_SEMVER}\n\nRunning on {get_native_host()}.")
+
+    if gc.is_installation_externally_managed:
+        print("This Ruyi installation is externally managed.")
+
+    print()
+
     log.stdout(COPYRIGHT_NOTICE)
 
     return 0
