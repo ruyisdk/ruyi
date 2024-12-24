73fcb14f9e9d556403e982812bffd1cde0e44b52
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-12-24 19:38:34 +0800

    feat(cli): add `ruyi config {set,unset,remove-section}`

--- ruyi/cli/config_cli.py
+++ ruyi/cli/config_cli.py
@@ -1,7 +1,8 @@
 import argparse
 
 from .. import config
-from ..config.schema import encode_value
+from ..config.schema import decode_value, encode_value
+from ..config.editor import ConfigEditor
 from .. import log
 from .cmd import RootCommand
 
@@ -38,4 +39,83 @@ class ConfigGetCommand(
             return 1
 
         log.stdout(encode_value(val))
+        return 0
+
+
+class ConfigSetCommand(
+    ConfigCommand,
+    cmd="set",
+    help="Set the value of a Ruyi config option",
+):
+    @classmethod
+    def configure_args(cls, p: argparse.ArgumentParser) -> None:
+        p.add_argument(
+            "key",
+            type=str,
+            help="The Ruyi config option to set",
+        )
+        p.add_argument(
+            "value",
+            type=str,
+            help="The value to set the option to",
+        )
+
+    @classmethod
+    def main(cls, cfg: config.GlobalConfig, args: argparse.Namespace) -> int:
+        key: str = args.key
+        val: str = args.value
+
+        pyval = decode_value(key, val)
+        with ConfigEditor.work_on_user_local_config(cfg) as ed:
+            ed.set_value(key, pyval)
+            ed.stage()
+
+        return 0
+
+
+class ConfigUnsetCommand(
+    ConfigCommand,
+    cmd="unset",
+    help="Unset a Ruyi config option",
+):
+    @classmethod
+    def configure_args(cls, p: argparse.ArgumentParser) -> None:
+        p.add_argument(
+            "key",
+            type=str,
+            help="The Ruyi config option to unset",
+        )
+
+    @classmethod
+    def main(cls, cfg: config.GlobalConfig, args: argparse.Namespace) -> int:
+        key: str = args.key
+
+        with ConfigEditor.work_on_user_local_config(cfg) as ed:
+            ed.unset_value(key)
+            ed.stage()
+
+        return 0
+
+
+class ConfigRemoveSectionCommand(
+    ConfigCommand,
+    cmd="remove-section",
+    help="Remove a section from the Ruyi config",
+):
+    @classmethod
+    def configure_args(cls, p: argparse.ArgumentParser) -> None:
+        p.add_argument(
+            "section",
+            type=str,
+            help="The section to remove",
+        )
+
+    @classmethod
+    def main(cls, cfg: config.GlobalConfig, args: argparse.Namespace) -> int:
+        section: str = args.section
+
+        with ConfigEditor.work_on_user_local_config(cfg) as ed:
+            ed.remove_section(section)
+            ed.stage()
+
         return 0
