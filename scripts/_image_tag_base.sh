51e2e5b2e498aa4d6f1ef8a9a1d6d7847c4a139a
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-03-29 00:31:49 +0800

    build: bump dist image, add the docker command and bump build deps

--- scripts/_image_tag_base.sh
+++ scripts/_image_tag_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this file is meant to be sourced
 
-_COMMON_DIST_IMAGE_TAG="ghcr.io/ruyisdk/ruyi-python-dist:20250107"
+_COMMON_DIST_IMAGE_TAG="ghcr.io/ruyisdk/ruyi-python-dist:20250329"
 
 # Map of `uname -m` outputs to Debian arch name convention which Ruyi adopts
 #
@@ -14,6 +14,7 @@ _COMMON_DIST_IMAGE_TAG="ghcr.io/ruyisdk/ruyi-python-dist:20250107"
 declare -A _UNAME_ARCH_MAP=(
     ["aarch64"]="arm64"
     ["i686"]="i386"
+    ["riscv64"]="riscv64"
     ["x86_64"]="amd64"
 )
 
