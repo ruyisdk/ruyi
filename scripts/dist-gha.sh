5be66545715eda11172feb4229beee1351a6d2a7
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-04-04 22:38:17 +0800

    ci: fix ownership of build cache directory

--- scripts/dist-gha.sh
+++ scripts/dist-gha.sh
@@ -9,4 +9,15 @@ export RUYI_DIST_BUILD_DIR=/github/workspace/build
 export RUYI_DIST_CACHE_DIR=/github/workspace/build-cache/ruyi-dist-cache
 export RUYI_DIST_INNER_CONTAINERIZED=x
 export RUYI_DIST_INNER=x
-exec "$MY_DIR"/dist.sh "$@"
+"$MY_DIR"/dist.sh "$@"
+ret=$?
+
+# fix the cache directory's ownership if necessary
+cache_uid="$(stat -c '%u' /github/workspace/build-cache)"
+workspace_uid="$(stat -c '%u' /github/workspace)"
+if [[ $cache_uid -ne $workspace_uid ]]; then
+    echo "fixing ownership of build cache directory"
+    chown -v --reference=/github/workspace /github/workspace/build-cache
+fi
+
+exit $ret
