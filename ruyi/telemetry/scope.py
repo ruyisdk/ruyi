670a8a9f2653e9e61097e4e3bae63b63ff82e98a
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-05-15 17:37:18 +0800

    feat(telemetry): make a data type for recording repo-level scopes
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- ruyi/telemetry/scope.py
+++ ruyi/telemetry/scope.py
@@ -11,3 +11,24 @@ def is_telemetry_scope_config(x: object) -> TypeGuard[TelemetryScopeConfig]:
             return True
         case _:
             return False
+
+
+class TelemetryScope:
+    def __init__(self, repo_name: str | None) -> None:
+        self._repo_name = repo_name
+
+    def __repr__(self) -> str:
+        return f"TelemetryScope(repo_name={self._repo_name})"
+
+    def __str__(self) -> str:
+        if self._repo_name:
+            return f"repo:{self._repo_name}"
+        return "pm"
+
+    @property
+    def repo_name(self) -> str | None:
+        return self._repo_name
+
+    @property
+    def is_pm(self) -> bool:
+        return self._repo_name is None
