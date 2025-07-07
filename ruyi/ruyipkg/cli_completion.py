e64f3b365efa64070efd87f246cdd09b1ba015ab
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-07-07 15:14:38 +0800

    style: format cli_completion.py with vscode
    Ruff didn't complain about this, but VSCode will reformat nevertheless.
    
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- ruyi/ruyipkg/cli_completion.py
+++ ruyi/ruyipkg/cli_completion.py
@@ -10,8 +10,12 @@ def package_completer_builder(
     filters: list[Callable[[str], bool]] | None = None,
 ) -> "DynamicCompleter":
     # Lazy import to avoid circular dependency
-    from ..ruyipkg.augmented_pkg import AugmentedPkg  # pylint: disable=import-outside-toplevel
-    from ..ruyipkg.list_filter import ListFilter  # pylint: disable=import-outside-toplevel
+    from ..ruyipkg.augmented_pkg import (
+        AugmentedPkg,
+    )  # pylint: disable=import-outside-toplevel
+    from ..ruyipkg.list_filter import (
+        ListFilter,
+    )  # pylint: disable=import-outside-toplevel
 
     all_pkgs = list(AugmentedPkg.yield_from_repo(cfg, cfg.repo, ListFilter()))
     if filters is not None:
