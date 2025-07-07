cbd4b2c5d29eb1153608931640f6334c4e98db98
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-07-07 16:25:13 +0800

    refactor(resource_bundle): move bundled assets out of Python package proper
    While at it, also allow bundling non-Jinja assets for future use cases.
    
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- ruyi/resource_bundle/__main__.py
+++ ruyi/resource_bundle/__main__.py
@@ -2,13 +2,12 @@
 # Regenerates data.py from fresh contents.
 
 import base64
-import glob
-import os.path
+import pathlib
 from typing import Any
 import zlib
 
 
-def make_payload_from_file(path: str) -> str:
+def make_payload_from_file(path: pathlib.Path) -> str:
     with open(path, "rb") as fp:
         content = fp.read()
 
@@ -16,11 +15,22 @@ def make_payload_from_file(path: str) -> str:
 
 
 def main() -> None:
-    self_path = os.path.dirname(__file__)
-    os.chdir(self_path)
+    self_path = pathlib.Path(__file__).parent.resolve()
+    bundled_resource_root = self_path / ".." / ".." / "resources" / "bundled"
 
-    payloads = {f[:-6]: make_payload_from_file(f) for f in glob.iglob("*.jinja")}
-    with open("data.py", "w", encoding="utf-8") as fp:
+    resources: dict[str, str] = {}
+    template_names: dict[str, str] = {}
+    for f in bundled_resource_root.iterdir():
+        if not f.is_file():
+            continue
+
+        resources[f.name] = make_payload_from_file(f)
+
+        if f.suffix.lower() == ".jinja":
+            # strip the .jinja suffix for the template name
+            template_names[f.stem] = f.name
+
+    with open(self_path / "data.py", "w", encoding="utf-8") as fp:
 
         def p(*args: Any) -> None:
             return print(*args, file=fp)
@@ -31,8 +41,13 @@ def main() -> None:
         p("from typing import Final\n\n")
 
         p("RESOURCES: Final = {")
-        for filename, payload in sorted(payloads.items()):
+        for filename, payload in sorted(resources.items()):
             p(f'    "{filename}": b"{payload}",  # fmt: skip')
+        p("}\n")
+
+        p("TEMPLATES: Final = {")
+        for stem, full_filename in sorted(template_names.items()):
+            p(f'    "{stem}": RESOURCES["{full_filename}"],')
         p("}")
 
 
