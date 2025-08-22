b5e447b449749abc04e099f0768a438f3cfa31b9
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-08-22 17:20:07 +0800

    build: bump dist image, bump Python to 3.13.7 and add ruyi-litester deps
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- scripts/dist-image/build-python.sh
+++ scripts/dist-image/build-python.sh
@@ -8,7 +8,7 @@ set -e
 #
 # See: https://github.com/Nuitka/Nuitka/commit/54f2a2222abedf92d45b8f397233cfb3bef340c5
 
-PYTHON_V=3.13.2
+PYTHON_V=3.13.7
 pushd /tmp
 wget "https://www.python.org/ftp/python/${PYTHON_V}/Python-${PYTHON_V}.tar.xz"
 mkdir py-src py-build
