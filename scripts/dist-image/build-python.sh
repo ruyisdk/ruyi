b07b8ec67d0be08448a71bb4841f285e4ba4c9b7
WANG Xuerui <wangxuerui@iscas.ac.cn>
2026-04-20 17:16:14 +0800

    build: bump dist image, bump to Ubuntu 24.04 LTS and Python 3.14.x
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- scripts/dist-image/build-python.sh
+++ scripts/dist-image/build-python.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# Nuitka now requires final versions of Python, but unfortunately the python3.11
-# in the jammy repo is 3.11.0rc1, and there's no python3.12 in repo, so we have
-# to build our own Python for now.
+# Nuitka now requires final versions of Python, and while a final version of
+# python3.12 is in repo, having a newer version would mean a better security
+# posture, and possibility to have static libpython linkage should needs arise.
 #
 # See: https://github.com/Nuitka/Nuitka/commit/54f2a2222abedf92d45b8f397233cfb3bef340c5
 
-PYTHON_V=3.13.9
+PYTHON_V=3.14.4
 pushd /tmp
 wget "https://www.python.org/ftp/python/${PYTHON_V}/Python-${PYTHON_V}.tar.xz"
 mkdir py-src py-build
--- scripts/dist-image/prepare-poetry.sh
+++ scripts/dist-image/prepare-poetry.sh
@@ -7,15 +7,15 @@ export MAKEFLAGS
 
 # poetry should be put into its own venv to avoid contaminating the dist build
 # venv; otherwise nuitka can and will see additional imports leading to bloat
-python3.13 -m venv /home/b/build-tools-venv
+python3.14 -m venv /home/b/build-tools-venv
 /home/b/build-tools-venv/bin/pip install -U pip setuptools wheel
 /home/b/build-tools-venv/bin/pip install poetry
-/home/b/build-tools-venv/bin/pip install maturin==1.9.6 cibuildwheel~=3.1.1 auditwheel==6.4.2
+/home/b/build-tools-venv/bin/pip install maturin==1.13.1 cibuildwheel~=3.4.1 auditwheel==6.6.0
 for tool in poetry maturin cibuildwheel auditwheel; do
     ln -s /home/b/build-tools-venv/bin/"$tool" /usr/local/bin/"$tool"
 done
 
-python3.13 -m venv /home/b/venv
+python3.14 -m venv /home/b/venv
 /home/b/venv/bin/pip install -U pip setuptools wheel
 chown -R "$BUILDER_UID:$BUILDER_GID" /home/b/venv
 
