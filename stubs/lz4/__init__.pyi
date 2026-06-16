a3767ea7de64234e2ebe37fb9d85f34a1d066dda
WANG Xuerui <wangxuerui@iscas.ac.cn>
2026-06-16 18:02:39 +0800

    build: stubgen lz4
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

---
+++ stubs/lz4/__init__.pyi
@@ -0,0 +1,4 @@
+from ._version import library_version_number as library_version_number, library_version_string as library_version_string
+from .version import version as __version__
+
+VERSION = __version__
---
+++ stubs/lz4/block/__init__.pyi
@@ -0,0 +1,1 @@
+from ._block import LZ4BlockError as LZ4BlockError, compress as compress, decompress as decompress
