302de425cc8cedaf3cd9c36fe00b2408442bfd94
WANG Xuerui <wangxuerui@iscas.ac.cn>
2026-06-22 15:44:32 +0800

    fix: bump dist image, restore Python 3.14.x and fix missing libbz2 and liblzma
    Fixes: #473
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>
    (cherry picked from commit 7efa9d31b73f55a3247bf463ccbf05aafe001002)

--- scripts/dist-image/prepare-poetry.sh
+++ scripts/dist-image/prepare-poetry.sh
@@ -5,18 +5,50 @@ set -e
 MAKEFLAGS="-j$(nproc)"
 export MAKEFLAGS
 
+_PIP_INSTALL=( pip install )
+
+# workaround too old rustc (1.81) when cryptography-49.0 has to be compiled,
+# with an MSRV of 1.83
+case "$(uname -m)" in
+riscv64)
+    wget https://static.rust-lang.org/rustup/dist/riscv64gc-unknown-linux-gnu/rustup-init
+    chmod a+x rustup-init
+    export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup/
+    ./rustup-init -y  # because there is already a system Rust
+    rustc -Vv
+    _PIP_INSTALL+=(
+        --prefer-binary
+        --extra-index-url https://gitlab.com/api/v4/projects/riseproject%2Fpython%2Fwheel_builder/packages/pypi/simple
+    )
+    ;;
+esac
+
+_pip_install() {
+    "${_PIP_INSTALL[@]}" "$@"
+}
+
 # poetry should be put into its own venv to avoid contaminating the dist build
 # venv; otherwise nuitka can and will see additional imports leading to bloat
-python3.13 -m venv /home/b/build-tools-venv
-/home/b/build-tools-venv/bin/pip install -U pip setuptools wheel
-/home/b/build-tools-venv/bin/pip install poetry
-/home/b/build-tools-venv/bin/pip install maturin==1.13.1 cibuildwheel~=3.4.1 auditwheel==6.6.0
+python3.14 -m venv /home/b/build-tools-venv
+# shellcheck disable=SC1091
+. /home/b/build-tools-venv/bin/activate
+hash -r
+_pip_install -U pip setuptools wheel
+_pip_install poetry
+_pip_install maturin==1.13.1 cibuildwheel~=3.4.1 auditwheel==6.6.0
+deactivate
+hash -r
 for tool in poetry maturin cibuildwheel auditwheel; do
     ln -s /home/b/build-tools-venv/bin/"$tool" /usr/local/bin/"$tool"
 done
 
-python3.13 -m venv /home/b/venv
-/home/b/venv/bin/pip install -U pip setuptools wheel
+python3.14 -m venv /home/b/venv
+# shellcheck disable=SC1091
+. /home/b/venv/bin/activate
+hash -r
+_pip_install -U pip setuptools wheel
+deactivate
+hash -r
 chown -R "$BUILDER_UID:$BUILDER_GID" /home/b/venv
 
 # remove wheel caches in the root user
