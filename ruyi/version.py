1b83266832d3fffed13584a67d357debff536289
WANG Xuerui <wangxuerui@iscas.ac.cn>
2026-08-28 10:21:55 +0800

    build: bump self version to 0.53.0-alpha.20260828
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- ruyi/version.py
+++ ruyi/version.py
@@ -2,7 +2,7 @@ from typing import Final
 
 from .i18n import d_
 
-RUYI_SEMVER: Final = "0.52.0-alpha.20260714"
+RUYI_SEMVER: Final = "0.53.0-alpha.20260828"
 RUYI_USER_AGENT: Final = f"ruyi/{RUYI_SEMVER}"
 
 COPYRIGHT_NOTICE: Final = d_(
